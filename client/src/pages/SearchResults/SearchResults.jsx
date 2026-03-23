5a9dbf42b6b63b1c926b3df8b2dd03091fedad60
Paywand Amir <paywanda@missionreadyhq.com>
2026-03-23 22:30:05 +1100

    added assets

--- client/src/pages/SearchResults/SearchResults.jsx
+++ client/src/pages/SearchResults/SearchResults.jsx
@@ -1,7 +1,29 @@
 import "./SearchResults.css"
+import Header from "../../common/Header"
+import Footer from "../../common/Footer"
 
-function SearchResults() {
-  return <div>SearchResults</div>;
+
+// https://img5.su-cdn.com/cdn-cgi/image/width=750,height=750/mall/file/2023/10/23/9e0f37a7886bdf2ccb4149b515b42a54.jpg
+
+function SearchResults()
+{
+  return (
+  <div className="searchResultsPage">
+    <Header/>
+    <div className="searchHeader">
+      <p className="paths">Home / Marketplace / Home & Living</p>
+      <p className="category">Home & Living</p>
+    </div>
+    
+    <div className="categories-filters">
+      Categories
+    </div>
+    <div className="searchResultsMain">
+      Search Results
+    </div>
+    <Footer/>
+  </div>
+  )
 }
 
 export default SearchResults;

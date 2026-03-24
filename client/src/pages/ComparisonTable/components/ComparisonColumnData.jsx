20a387e5c4317df101fb21a865c9c3be471d4dfc
Lharyzza <lharyzzav@missionreadyhq.com>
2026-03-24 13:22:07 +1300

    initialized ColumnDisplay and module.css. Removed functions displaying data from Comparison Column data and plcaed into column display

--- client/src/pages/ComparisonTable/components/ComparisonColumnData.jsx
+++ client/src/pages/ComparisonTable/components/ComparisonColumnData.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styles from "./ComparisonColumnData.module.css";
+import ColumnDisplay from "./ColumnDisplay";
 
 function ComparisonColumnData() {
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:3000/comparisontable/compare")
+    fetch("http://localhost:3000/comparisontable")
       .then((response) => response.json())
       .then((items) => {
         console.log("Fetched data:", items);
@@ -20,46 +21,7 @@ function ComparisonColumnData() {
 
   return (
     <div className={styles.columnContainer}>
-      <h1>Comparison Table: Wooden Desks</h1>
-
-      {error && <p>{error}</p>}
-
-      <table className={styles.comparisonTable}>
-        <thead className={styles.tableHead}>
-          <tr>
-            <th className={styles.titleFont}>Title</th>
-            <th>Image</th>
-            <th>Price</th>
-            <th>Condition</th>
-            <th>Dimensions</th>
-            <th>Shipping & Pick Up</th>
-            <th>Payment Options</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {data.map((rowItem) => (
-            <tr key={rowItem._id} className={styles.tableRow}>
-              <td>{rowItem.title}</td>
-              <td>
-                <img
-                  className={styles.img}
-                  src={rowItem.image}
-                  alt={rowItem.name}
-                />
-              </td>
-              <td>
-                Buy now <br />
-                {rowItem.price}
-              </td>
-              <td>{rowItem.condition}</td>
-              <td>{rowItem.dimensions}</td>
-              <td>{rowItem.shipping_and_pickup}</td>
-              <td>{rowItem.payment_options}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <ColumnDisplay data={data} />
     </div>
   );
 }

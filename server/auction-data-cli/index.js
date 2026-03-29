d15ed701898a7553ad1e43e2168db4fa6beb9e65
Lharyzza <lharyzzav@missionreadyhq.com>
2026-03-30 12:23:05 +1300

    removed unused models page from server and updated seedData

--- server/auction-data-cli/index.js
+++ server/auction-data-cli/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: "../.env" });
 const mongoose = require("mongoose");
-const Item = require("./models/itemSchema");
+const Item = require("../models/itemModel");
 const connectDb = require("../config/db");
 const seedData = require("./data/seedData");
 
